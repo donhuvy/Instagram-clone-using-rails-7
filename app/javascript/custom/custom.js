e7552360264ea69a0ce1605729985bb17cb285b8
Vy Do <donhuvy@outlook.com>
2024-05-05 19:54:56 +0700

    format

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -9,20 +9,20 @@ import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
 document.addEventListener("turbo:load", loadFilePond);
 
 function loadFilePond() {
-  FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginFileValidateType
-  );
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginFileValidateType
+    );
 
-  // Get a reference to the file input element
-  const inputElement = document.querySelector("#post-images");
+    // Get a reference to the file input element
+    const inputElement = document.querySelector("#post-images");
 
-  // Create a FilePond instance
-  const pond = FilePond.create(inputElement, {
-    credits: {},
-    storeAsFile: true,
-    allowMultiple: true,
-    allowReorder: true,
-    acceptedFileTypes: ["image/*"],
-  });
+    // Create a FilePond instance
+    const pond = FilePond.create(inputElement, {
+        credits: {},
+        storeAsFile: true,
+        allowMultiple: true,
+        allowReorder: true,
+        acceptedFileTypes: ["image/*"],
+    });
 }
